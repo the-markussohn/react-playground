4013cd4bc984b6a1b2c256c9126f6373a7330997
the-markussohn <markus.justinas@gmail.com>
2018-08-29 16:48:38 +0300

    flexible lists

--- src/App.js
+++ src/App.js
@@ -44,22 +44,24 @@ class App extends Component {
         });
     };
 
-    nameChangedHandler = (event) => {
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex((p) => {
+            return p.id === id;
+        });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+
+        persons[personIndex] = person;
+
         this.setState({
-            persons: [
-                {
-                    name: 'Max',
-                    age: 28
-                },
-                {
-                    name: event.target.value,
-                    age: 29
-                },
-                {
-                    name: 'Stephanie',
-                    age: 27
-                }
-            ]
+            persons
         });
     };
 
@@ -100,6 +102,7 @@ class App extends Component {
                                 name={person.name}
                                 age={person.age}
                                 key={person.id}
+                                changed={(event) => this.nameChangedHandler(event, person.id)}
                             />
                         );
                     })}
