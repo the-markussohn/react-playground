402010b4ef8392b9f9a422a81ff7233a37da0f91
the-markussohn <markus.justinas@gmail.com>
2018-08-31 13:54:58 +0300

    internal update

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,6 +26,19 @@ class App extends Component {
         console.log('[App.js] inside componentDidMount()');
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[UPDATE App.js] inside shouldComponentUpdate', nextProps, nextState);
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE App.js] inside componentWillUpdate', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('[UPDATE App.js] inside componentDidUpdate');
+    }
+
     nameChangedHandler = (event, id) => {
         const personIndex = this.state.persons.findIndex((p) => {
             return p.id === id;
