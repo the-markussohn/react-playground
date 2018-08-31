3efa42f27b10940101f0d5ac7fc8a31d566eca6f
the-markussohn <markus.justinas@gmail.com>
2018-08-31 15:41:45 +0300

    wrapping component as class

--- src/hoc/withClassName.js
+++ src/hoc/withClassName.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+// const withClassName = (WrappedComponent, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props}/>
+//         </div>
+//     );
+// };
 
 const withClassName = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent {...props}/>
-        </div>
-    );
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props}/>
+                </div>
+            );
+        }
+    };
 };
 
 export default withClassName;
